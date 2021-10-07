4ab9a987b18840f4b5d94901b680e992ad5cd1b0
pacchione-t <79484899+pacchione-t@users.noreply.github.com>
2021-10-07 16:02:03 -0400

    Add files via upload

---
+++ MazePlayer.java
@@ -0,0 +1,11 @@
+package mazegame;
+
+// Facilitates the user experience of playing the maze game
+public class MazePlayer {
+	public static void main(String[] args) {
+	    Maze maze = new Maze(40, 25);
+	    
+	    // Call the makeGame function on a maze instance to play the game
+	    maze.makeGame();
+	}
+}
